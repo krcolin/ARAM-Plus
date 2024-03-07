227e00b9d0e34f2c2d9d19442745cf47fc830a3a
Colin <kreeftcolin@gmail.com>
2024-03-07 21:16:47 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,148 +1,157 @@
 # -*- coding: utf-8 -*-
 import random
-from data.data import champions, logo
-import os
-import ctypes
+from data.data import champions
+import tkinter as tk
+from tkinter import messagebox, simpledialog
+from PIL import Image, ImageTk
 
-# CHAT-GPT Generated code to change cmd prompt design
-ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004
-STD_OUTPUT_HANDLE = -11
-handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
-mode = ctypes.c_ulong()
-ctypes.windll.kernel32.GetConsoleMode(handle, ctypes.byref(mode))
-mode.value |= ENABLE_VIRTUAL_TERMINAL_PROCESSING
-ctypes.windll.kernel32.SetConsoleMode(handle, mode)
-systeem = os.system('color F0')
+PLAYERS = simpledialog.askinteger("Input", "How many players?", minvalue=2, maxvalue=10)
+FONT = ("Helvetica", 18)
+AMT_CHMPS = 3
 
-# Colors for cmd
-RED = "\033[31m"
-GREEN = "\033[32m"
-BLUE = "\033[34m"
-RESET = "\033[30m"
-
-print(logo)
+img = Image.open("tk_logo.png")
+img = img.reduce(5)
 
 
-# Function to get 6 names from input
-def get_names():
-    """
-    A function that prompts the user to enter 6 names separated by commas, then returns the names as a list if there are exactly 6 names entered. Otherwise, it prints an error message and continues prompting for input.
+def center_window(root):
+    # Update the window to make sure we have the latest dimensions
+    root.update_idletasks()
 
-    :return: list: The list of names entered by the user.
-    """
-    while True:
-        input_names = input(f"Enter 6 namen met een , er tussen: ")
-        namen = [name.strip() for name in input_names.split(',')]
-        if len(namen) == 6:
-            return namen
-        else:
-            print("Error: Vul 6 namen in.")
+    # Get the window dimensions
+    width = root.winfo_width()
+    height = root.winfo_height()
 
+    # Get the screen width and height
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
 
-namen = get_names()
+    # Calculate the x and y coordinates based on the screen dimensions
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
 
-
-def main():
-    random.shuffle(namen)
-
-    team1 = namen[0:3]
-    team2 = namen[3:]
-    f_team1 = ", ".join(team1).title()
-    f_team2 = ", ".join(team2).title()
-    print(f"\n{BLUE}Team Blue: {f_team1}\n{RED}Team Red: {f_team2}")
-
-    champions_per_player = assign_champions_interactively(team1, team2, champions)
-
-    print_teams_with_champions(team1, team2, champions_per_player)
+    # Set the position of the window at the center of the screen
+    root.geometry(f'+{x}+{y}')
+    root.deiconify()
 
 
-def assign_champions_interactively(team1: list, team2: list, champions: list):
-    """
-    Assigns champions interactively to two teams of players from a pool of available champions.
+root = tk.Tk()
+root.title("ARAM 3v3 Custom Generator")
+root.configure(padx=50, pady=50)
 
-    :param team1: List of players in team 1.
-    :param team2: List of players in team 2.
-    :param champions: List of available champions.
-    :return: A dictionary containing the allocation of champions to each player in the format {player: "Champion 1, Champion 2, ..."}.
-    :rtype: dict
-    """
-    global count
-    champions_pool = champions[:]
-    champion_allocation = {player: [] for player in team1 + team2}
-    count = 0
+logo = ImageTk.PhotoImage(img)
 
-    def player_select_champion(player: str, champions_pool: list):
-        """
-        Selects a champion for the player from the given champions pool.
+title = tk.Label(root, text="ARAM 3v3 Custom Generator", font=FONT)
+logo_label = tk.Label(root, image=logo)
 
-        :param player: The player for whom the champion is being selected.
-        :param champions_pool: The pool of champions to select from.
-        :return: The chosen champion selected from the pool.
-        :rtype: str
-        """
-        global count
-        if count % 2 == 0:
-            kleur = BLUE
-        else:
-            kleur = RED
-        available_choices = random.sample(champions_pool, 3)
-        print(f"\n{kleur}{player.title()}'s keuzes:")
-        for idx, champ in enumerate(available_choices, start=1):
-            print(f"\t{idx}. {champ}")
-        while True:
-            try:
-                choice = int(input(f"{GREEN}Welk nummer: ")) - 1
-                if 0 <= choice < 3:
-                    chosen_champion = available_choices[choice]
-                    champions_pool.remove(chosen_champion)
-                    break
-                else:
-                    print("Kies een nummer tussen 1 en 3.")
-            except ValueError:
-                print("Voer a.u.b. een geldig nummer in.")
-        count += 1
-        return chosen_champion
+logo_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
-    players_order = []
-    for player1, player2 in zip(team1, team2):
-        players_order.append(player1)
-        players_order.append(player2)
+title.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="nswe")
+names_input = tk.Canvas(root, width=400, height=300)
+names_input.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+champions_pool = champions
+names_entries = []
+names = []
+player_champion = {}
 
-    for _ in range(1):
-        for player in players_order:
-            chosen_champion = player_select_champion(player, champions_pool)
-            champion_allocation[player].append(chosen_champion)
-
-    for player in champion_allocation:
-        champion_allocation[player] = ', '.join(champion_allocation[player]).title()
-
-    return champion_allocation
+for index, _ in enumerate(range(PLAYERS)):
+    name = tk.Entry(names_input)
+    name.grid(row=index, column=0, padx=10, pady=10)
+    names_entries.append(name)
 
 
-def print_teams_with_champions(team1: list, team2: list, champion_allocation: dict):
-    """
-    Print teams with champions and their allocations.
-
-    :param team1: list of strings representing the players in the blue team
-    :param team2: list of strings representing the players in the red team
-    :param champion_allocation: dictionary mapping player names to their allocated champions
-
-    """
-    print(f"\n{BLUE}Blue:")
-    for player in team1:
-        print(f"\t{player.title()}: {champion_allocation[player]}")
-
-    print(f"\n{RED}Red:")
-    for player in team2:
-        print(f"\t{player.title()}: {champion_allocation[player]}")
+def create_champion_selection_area():
+    for widget in champion_selection_frame.winfo_children():
+        widget.destroy()
+    if current_player in team1:
+        color = "blue"
+    else:
+        color = "red"
 
 
-if __name__ == "__main__":
-    while True:
-        main()
-        prompt = input(f"\n{RESET}Enter voor reroll, 'namen' om nieuwe "
-                       f"namen toe te voegen of 'exit' om te stoppen... ").lower()
-        if prompt == 'exit':
-            break
-        if prompt == 'namen':
-            namen = get_names()
+    tk.Label(champion_selection_frame, text=f"{current_player}'s Turn to Choose a Champion:", fg=color,
+             font=FONT).grid(row=0, column=0, padx=10, pady=10)
+
+    champion_choices = random.sample(champions_pool, AMT_CHMPS)
+    for idx, champ in enumerate(champion_choices):
+        idx = idx + 1
+        btn = tk.Button(champion_selection_frame, width=10, text=champ, command=lambda c=champ: select_champion(c))
+        btn.grid(row=idx, column=0, padx=10, pady=5)
+
+
+def select_champion(champ):
+    global current_player_index, interleaved_players
+    player_champion[current_player] = champ  # Map the player to their selected champion.
+    champions_pool.remove(champ)  # Remove the selected champion from the pool.
+
+    # Update the corresponding label for the current player.
+    for name_label in name_labels:
+        cur_name = name_label.cget("text")
+        if current_player == cur_name:
+            name_label.config(text=f"{cur_name}: {champ}")
+
+    current_player_index += 1
+    if current_player_index < len(interleaved_players):
+        update_current_player()
+        create_champion_selection_area()
+    else:
+        champion_selection_frame.destroy()
+
+
+def update_current_player():
+    global current_player
+    current_player = interleaved_players[current_player_index]
+
+
+def submit_names():
+    global names, current_player, interleaved_players, current_player_index
+    names = [name_entry.get() for name_entry in names_entries if name_entry.get() != ""]
+    if len(names) < PLAYERS:
+        messagebox.showerror("Error", "One or more names are empty.")
+        return
+    else:
+        # Destroy the names input canvas and submit button after successful submission
+        names_input.destroy()
+        submit_button.destroy()
+        initialize_champion_selection()
+
+
+submit_button = tk.Button(names_input, text="Submit", command=submit_names)
+submit_button.grid(row=10, column=0, columnspan=2, padx=10, pady=10)
+root.withdraw()
+root.after(100, center_window, root)
+
+name_labels = []
+
+
+def initialize_champion_selection():
+    global champions_pool, player_champion, interleaved_players, current_player, current_player_index,\
+        champion_selection_frame, team1 ,team2
+    # Shuffle names and prepare interleaved player list for turn-based selection
+    random.shuffle(names)
+    players_per_team = len(names) // 2
+    team1 = names[:players_per_team]
+    team2 = names[players_per_team:]
+    interleaved_players = [val for pair in zip(team1, team2) for val in pair]
+    current_player_index = 0
+    current_player = interleaved_players[current_player_index]
+
+    # Initialize the champion selection UI
+    champion_selection_frame = tk.Frame(root)
+    teams_frame = tk.Frame(root)
+    teams_frame.grid(row=1, column=0, padx=10, pady=10, sticky="nswe", columnspan=2)
+    teams_frame.columnconfigure(0, weight=1)
+    teams_frame.columnconfigure(1, weight=1)
+    for idx, player in enumerate(team1):
+        player_label = tk.Label(teams_frame, text=player, font=FONT, fg="blue")
+        player_label.grid(column=0, row=idx, sticky="ew")
+        name_labels.append(player_label)
+    for idx, player in enumerate(team2):
+        player_label = tk.Label(teams_frame, text=player, font=FONT, fg="red")
+        player_label.grid(column=1, row=idx, sticky="ew")
+        name_labels.append(player_label)
+    champion_selection_frame.grid(row=2, column=0, columnspan=2, padx=10, pady=10, sticky="nswe")
+    champion_selection_frame.configure(highlightbackground="grey", highlightthickness=1, relief="raised")
+    create_champion_selection_area()
+
+
+root.mainloop()
